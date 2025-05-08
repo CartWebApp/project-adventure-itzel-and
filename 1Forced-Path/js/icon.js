05f3cec7ac415353ebc0f60758d30442fd5a9a5f
ItzelG07 <itzel.gomez24@student.cart.org>
2025-05-08 10:06:21 -0700

    icon tab index page

--- 1Forced-Path/js/icon.js
+++ 1Forced-Path/js/icon.js
@@ -6,7 +6,7 @@
 var link = document.createElement('link');
 link.rel = 'icon';
 link.type = 'image/jpeg';  // Specify the type as JPEG (or "image/jpg")
-link.href = 'assets/icon.jpg'; // Path to the image in the 'assets' folder
+link.href = '/intro/images/andy-walking.gif'; // Path to the image in the 'assets' folder
 
 // Append the <link> element to the <head> section
 document.head.appendChild(link);
